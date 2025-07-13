71a95e9848e67e26a154c71a28bd965659a6d177
Ephraim Chuah <clperp.ec@gmail.com>
2025-07-13 20:20:03 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, U-Earth is Online! 🌍</h1>
+      <p>This is your starter page.</p>
+    </div>
+  );
+}
